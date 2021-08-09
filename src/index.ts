cbccd971cbf6342726a4081234667f5f3f9c1256
y9yk <andrew.yk82@gmail.com>
2021-08-09 11:06:48 +0900

    add metadata prop.

--- src/index.ts
+++ src/index.ts
@@ -7,98 +7,100 @@ const storage: (options?:ConfigurationObject)=>Storage = require('@google-cloud/
 
 export default class MulterGoogleCloudStorage implements multer.StorageEngine {
 
-	private gcobj: Storage;
-	private gcsBucket: Bucket;
-	private options: ConfigurationObject & { acl?: string, bucket?: string, contentType?: ContentTypeFunction };
+  private gcobj: Storage;
+  private gcsBucket: Bucket;
+  private options: ConfigurationObject & { acl?: string, bucket?: string, contentType?: ContentTypeFunction };
 
-	getFilename(req, file, cb) {
-    	cb(null,`${uuid()}_${file.originalname}`);
-	}
-	getDestination( req, file, cb ) {
-		cb( null, '' );
-	}
+  getFilename(req, file, cb) {
+      cb(null,`${uuid()}_${file.originalname}`);
+  }
+  getDestination( req, file, cb ) {
+    cb( null, '' );
+  }
 
-	public getContentType: ContentTypeFunction = (req, file) => {
-		return undefined;
-	}
+  public getContentType: ContentTypeFunction = (req, file) => {
+    return undefined;
+  }
 
-	constructor(opts?: ConfigurationObject & { filename?: any, bucket?:string, contentType?: ContentTypeFunction }) {
-		 opts = opts || {};
+  constructor(opts?: ConfigurationObject & { filename?: any, bucket?:string, contentType?: ContentTypeFunction }) {
+     opts = opts || {};
 
-		this.getFilename = (opts.filename || this.getFilename);
-		this.getContentType = (opts.contentType || this.getContentType);
+    this.getFilename = (opts.filename || this.getFilename);
+    this.getContentType = (opts.contentType || this.getContentType);
 
-		opts.bucket = (opts.bucket || process.env.GCS_BUCKET || null);
-		opts.projectId = opts.projectId || process.env.GCLOUD_PROJECT || null;
-		opts.keyFilename = opts.keyFilename || process.env.GCS_KEYFILE || null;
+    opts.bucket = (opts.bucket || process.env.GCS_BUCKET || null);
+    opts.projectId = opts.projectId || process.env.GCLOUD_PROJECT || null;
+    opts.keyFilename = opts.keyFilename || process.env.GCS_KEYFILE || null;
 
-		if (!opts.bucket) {
-			throw new Error('You have to specify bucket for Google Cloud Storage to work.');
-		}
+    if (!opts.bucket) {
+      throw new Error('You have to specify bucket for Google Cloud Storage to work.');
+    }
 
-		if (!opts.projectId) {
-			throw new Error('You have to specify project id for Google Cloud Storage to work.');
-		}
+    if (!opts.projectId) {
+      throw new Error('You have to specify project id for Google Cloud Storage to work.');
+    }
 
-		if (!opts.keyFilename) {
-			throw new Error('You have to specify credentials key file for Google Cloud Storage to work.');
-		}
+    if (!opts.keyFilename) {
+      throw new Error('You have to specify credentials key file for Google Cloud Storage to work.');
+    }
 
-		this.gcobj = storage({
-			projectId: opts.projectId,
-			keyFilename: opts.keyFilename
-		});
+    this.gcobj = storage({
+      projectId: opts.projectId,
+      keyFilename: opts.keyFilename
+    });
 
-		this.gcsBucket = this.gcobj.bucket(opts.bucket);
+    this.gcsBucket = this.gcobj.bucket(opts.bucket);
 
-		this.options = opts;
-	}
+    this.options = opts;
+  }
 
-	_handleFile = (req, file, cb) => {
-		this.getDestination(req, file, (err, destination) => {
+  _handleFile = (req, file, cb) => {
+    this.getDestination(req, file, (err, destination) => {
 
-			if (err) {
-				return cb(err);
-			}
+      if (err) {
+        return cb(err);
+      }
 
-			this.getFilename(req, file, (err, filename) => {
-				if (err) {
-					return cb(err);
-				}
-				var gcFile = this.gcsBucket.file(filename);
+      this.getFilename(req, file, (err, filename) => {
+        if (err) {
+          return cb(err);
+        }
+        var gcFile = this.gcsBucket.file(filename);
 
-				const streamOpts: Storage.WriteStreamOptions = {
-					predefinedAcl: this.options.acl || 'private'
-				};
+        const streamOpts: Storage.WriteStreamOptions = {
+          predefinedAcl: this.options.acl || 'private'
+        };
 
-				const contentType = this.getContentType(req, file);
+        const contentType = this.getContentType(req, file);
 
-				if (contentType) {
-				  streamOpts.metadata = {contentType};
-				}
+        if (contentType) {
+          streamOpts.metadata = {contentType};
+        }
 
-				file.stream.pipe(
-					gcFile.createWriteStream(streamOpts))
-					.on('error', (err) => cb(err))
-					.on('finish', (file) => cb(null, {
-							path: `https://${this.options.bucket}.storage.googleapis.com/${filename}`,
-							filename: filename,
+        file.stream.pipe(
+          gcFile.createWriteStream(streamOpts))
+          .on('error', (err) => cb(err))
+          .on('finish', (file) => {
+            cb(null, {
+              path: `https://${this.options.bucket}.storage.googleapis.com/${filename}`,
+              filename: filename,
               size: gcFile.metadata.size,
-						})
-					);
-			});
+              metadata: gcFile.metadata
+            });
+          });
+      });
 
-		});
-	}
-	_removeFile =  (req, file, cb) => {
-		var gcFile = this.gcsBucket.file(file.filename);
-		gcFile.delete();
-	};
+    });
+  }
+  _removeFile =  (req, file, cb) => {
+    var gcFile = this.gcsBucket.file(file.filename);
+    gcFile.delete();
+  };
 }
 
 export function storageEngine(opts?: ConfigurationObject & { filename?: any, bucket?:string }){
 
-	return new MulterGoogleCloudStorage(opts);
+  return new MulterGoogleCloudStorage(opts);
 }
 
 export type ContentTypeFunction = (req: Request, file: Express.Multer.File) => string | undefined;
